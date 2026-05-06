97f11ec70874a9bc0e3af194b9e50b1e66f8e118
maco-deep <onepi3celov3r@gmail.com>
2026-05-05 19:05:54 -0700

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Pura Fresada Belica.",
+        name: "Pura fresada belica🍓",
         // Activity type number (0 = Playing).
         type: 4, 
       },
